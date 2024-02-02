c97afdb068d836f124dc3a4fc502260aca3213c3
Diego Stamigni <diego.stamigni@gmail.com>
2024-02-02 11:56:23 +0000

    Update for 4.0.0.a2

--- generate.sh
+++ generate.sh
@@ -2,10 +2,10 @@
 #
 rm -rf .tmp/ || true
 
-TAG_VERSION="v3.6.0.b10"
-IOS_URL="https://download.videolan.org/pub/cocoapods/unstable/MobileVLCKit-3.6.0b10-615f96dc-4733d1cc.tar.xz"
-MACOS_URL="https://download.videolan.org/pub/cocoapods/unstable/VLCKit-3.6.0b10-615f96dc-4733d1cc.tar.xz"
-TVOS_URL="https://download.videolan.org/cocoapods/unstable/TVVLCKit-3.6.0b10-615f96dc-4733d1cc.tar.xz"
+TAG_VERSION="v4.0.0a2"
+IOS_URL="https://download.videolan.org/cocoapods/unstable/MobileVLCKit-4.0.0a2-b9e9412c-105babe3.tar.xz"
+MACOS_URL="https://download.videolan.org/cocoapods/unstable/VLCKit-4.0.0a2-b9e9412c-105babe3.tar.xz"
+TVOS_URL="https://download.videolan.org/cocoapods/unstable/TVVLCKit-4.0.0a2-b9e9412c-105babe3.tar.xz"
 
 mkdir .tmp/
 
@@ -33,10 +33,10 @@ xcodebuild -create-xcframework \
     -debug-symbols "${PWD}/$TVOS_LOCATION/tvos-arm64_x86_64-simulator/dSYMs/TVVLCKit.framework.dSYM" \
     -framework "$TVOS_LOCATION/tvos-arm64/TVVLCKit.framework"  \
     -debug-symbols "${PWD}/$TVOS_LOCATION/tvos-arm64/dSYMs/TVVLCKit.framework.dSYM" \
-    -framework "$IOS_LOCATION/ios-arm64_i386_x86_64-simulator/MobileVLCKit.framework" \
-    -debug-symbols "${PWD}/$IOS_LOCATION/ios-arm64_i386_x86_64-simulator/dSYMs/MobileVLCKit.framework.dSYM" \
-    -framework "$IOS_LOCATION/ios-arm64_armv7_armv7s/MobileVLCKit.framework" \
-    -debug-symbols "${PWD}/$IOS_LOCATION/ios-arm64_armv7_armv7s/dSYMs/MobileVLCKit.framework.dSYM" \
+    -framework "$IOS_LOCATION/ios-arm64_x86_64-simulator/MobileVLCKit.framework" \
+    -debug-symbols "${PWD}/$IOS_LOCATION/ios-arm64_x86_64-simulator/dSYMs/MobileVLCKit.framework.dSYM" \
+    -framework "$IOS_LOCATION/ios-arm64_armv7/MobileVLCKit.framework" \
+    -debug-symbols "${PWD}/$IOS_LOCATION/ios-arm64_armv7/dSYMs/MobileVLCKit.framework.dSYM" \
     -output .tmp/VLCKit-all.xcframework
     
 ditto -c -k --sequesterRsrc --keepParent ".tmp/VLCKit-all.xcframework" ".tmp/VLCKit-all.xcframework.zip"
